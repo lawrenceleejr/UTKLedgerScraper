f082bea187839ef4f389ba84e400d22754196bd2
Tova Holmes <tova.r.holmes@gmail.com>
2024-07-09 16:02:08 -0400

    Made the E account parsing more robust.

--- AccountReport.py
+++ AccountReport.py
@@ -15,6 +15,7 @@ def addZerosForMissingFields(s,whitespaceWidth=15):
 
 
 def cleanAndCastNumber(s):
+    if s.isspace() or s=="": return 0
     s = s.replace(",","")
     prefactor = 1
     if "(" in s and ")" in s:
@@ -48,18 +49,32 @@ class AccountReport:
         totalsLineNumber = -1
         for i,line in enumerate(self.splitText):
             # print(f"{i}: {line}")
-            if "TOTALCOSTS" in line:
+            if "TOTALCOSTS" in line or "TOTALYEAR-TO-DATECOSTS" in line:
                 totalsLineNumber = i
 
         # Parse the ledger reports for start-up differently
         if self.splitText[0].startswith("U013410060"):
-            summary_line = self.rawText.splitlines()[20].split()
-            self.totalEncumbrances = cleanAndCastNumber(summary_line[6])
-            self.totalSponsorBudget = cleanAndCastNumber(summary_line[5])
-            self.totalCurrentPeriodExpenditures = cleanAndCastNumber(summary_line[3]+summary_line[4])
-            self.totalCumulativeExpenditures = cleanAndCastNumber(summary_line[7])
+
+            # Do some god-awful parsing
+            block_size = 14
+            summary_numbers = self.rawText.splitlines()[totalsLineNumber].strip("TOTAL YEAR-TO-DATE COSTS").strip()
+            if len(summary_numbers)<=block_size*4: # Handle the case for no expenditures that month
+                current_month = ""
+                remaining_summary = summary_numbers
+            else:
+                current_month = summary_numbers.split(".")[0] + "." + summary_numbers.split(".")[1][0:2]
+                remaining_summary = summary_numbers.split(current_month)[1]
+            self.totalEncumbrances = cleanAndCastNumber(remaining_summary[block_size:2*block_size])
+            self.totalSponsorBudget = cleanAndCastNumber(remaining_summary[0:block_size])
+            self.totalCurrentPeriodExpenditures = cleanAndCastNumber(current_month.replace(" ",""))
+            self.totalCumulativeExpenditures = cleanAndCastNumber(remaining_summary[2*block_size:3*block_size])
             self.totalManualFundsReservations = 0
             self.totalUnderOverSpent = 0
+            #print(summary_numbers)
+            #print(current_month)
+            #print(remaining_summary)
+            #print(remaining_summary[0:block_size], remaining_summary[block_size:2*block_size], remaining_summary[2*block_size:3*block_size])
+            #print(self.totalCurrentPeriodExpenditures, self.totalSponsorBudget, self.totalEncumbrances, self.totalCumulativeExpenditures)
 
         else:
             self.obligatedAmount = cleanAndCastNumber(self.splitText[7].split()[-1])
